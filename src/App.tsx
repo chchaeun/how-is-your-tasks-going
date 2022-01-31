2e9376cd9adfbb5fd46996bf667c75fe81bcd57e
Kim Chae Eun <kce3615@naver.com>
2022-01-31 14:49:03 +0900

    remove: old version

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import React from "react";
 import Home from "./Home";
-import ToDoList from "./OldVersion/ToDoList";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 function App() {
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route path="/old-version">
-          <ToDoList />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
